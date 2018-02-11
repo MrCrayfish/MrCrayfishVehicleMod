02bdd069c89109db18d45d9b6e782362f7fc71ef
MrCrayfish <mrcrayfish@hotmail.com>
2018-02-11 21:22:17 +1030

    Incremented version

--- src/main/java/com/mrcrayfish/vehicle/Reference.java
+++ src/main/java/com/mrcrayfish/vehicle/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 {
     public static final String MOD_ID = "vehicle";
     public static final String MOD_NAME = "Vehicle Mod";
-    public static final String MOD_VERSION = "0.1.0";
+    public static final String MOD_VERSION = "0.2.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
 
     public static final String PROXY_CLIENT = "com.mrcrayfish.vehicle.proxy.ClientProxy";
