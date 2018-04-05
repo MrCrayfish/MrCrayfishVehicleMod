882bcbf33690a5b0efcb7a211adec5d50cdf1520
MrCrayfish <mrcrayfish@hotmail.com>
2018-04-05 14:24:31 +0930

    Incremented version

--- src/main/java/com/mrcrayfish/vehicle/Reference.java
+++ src/main/java/com/mrcrayfish/vehicle/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 {
     public static final String MOD_ID = "vehicle";
     public static final String MOD_NAME = "Vehicle Mod";
-    public static final String MOD_VERSION = "0.2.0";
+    public static final String MOD_VERSION = "0.3.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
     public static final String MOD_DEPENDS = "required-after:obfuscate@[0.2.3,)";
 
