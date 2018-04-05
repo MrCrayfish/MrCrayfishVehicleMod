8df0e48d24226533534b6213f0bee77079e5a923
MrCrayfish <mrcrayfish@hotmail.com>
2018-04-05 23:35:07 +0930

    Fixed "idle" sound showing as missing mapping

--- src/main/java/com/mrcrayfish/vehicle/client/ClientEvents.java
+++ src/main/java/com/mrcrayfish/vehicle/client/ClientEvents.java
@@ -1,19 +1,34 @@
 package com.mrcrayfish.vehicle.client;
 
+import com.google.common.collect.ImmutableList;
 import com.mrcrayfish.obfuscate.client.event.ModelPlayerEvent;
+import com.mrcrayfish.vehicle.Reference;
 import com.mrcrayfish.vehicle.entity.EntityATV;
 import com.mrcrayfish.vehicle.entity.EntityVehicle;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelPlayer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.List;
 
 /**
  * Author: MrCrayfish
  */
 public class ClientEvents
 {
+    private static final List<String> IGNORE_SOUNDS;
+
+    static
+    {
+        ImmutableList.Builder<String> builder = ImmutableList.builder();
+        builder.add("idle");
+        IGNORE_SOUNDS = builder.build();
+    }
+
     @SubscribeEvent
     public void onSetupAngles(ModelPlayerEvent.SetupAngles.Post event)
     {
@@ -43,6 +58,18 @@ public class ClientEvents
             model.bipedRightLeg.rotateAngleY = (float) Math.toRadians(30F);
             model.bipedLeftLeg.rotateAngleX = (float) Math.toRadians(-65F);
             model.bipedLeftLeg.rotateAngleY = (float) Math.toRadians(-30F);
+        }
+    }
+
+    @SubscribeEvent
+    public void onMissingMap(RegistryEvent.MissingMappings<SoundEvent> event)
+    {
+        for(RegistryEvent.MissingMappings.Mapping<SoundEvent> missing : event.getMappings())
+        {
+            if(missing.key.getResourceDomain().equals(Reference.MOD_ID) && IGNORE_SOUNDS.contains(missing.key.getResourcePath()))
+            {
+                missing.ignore();
+            }
         }
     }
 }
