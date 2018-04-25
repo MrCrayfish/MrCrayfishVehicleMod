2c94cfd487d3b562be455ced94a156d7fd481714
MrCrayfish <mrcrayfish@hotmail.com>
2018-04-25 12:07:53 +0930

    Added more missing mappings

--- src/main/java/com/mrcrayfish/vehicle/common/CommonEvents.java
+++ src/main/java/com/mrcrayfish/vehicle/common/CommonEvents.java
@@ -2,7 +2,7 @@ package com.mrcrayfish.vehicle.common;
 
 import com.google.common.collect.ImmutableList;
 import com.mrcrayfish.vehicle.Reference;
-import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
 import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,23 +15,42 @@ import java.util.List;
  */
 public class CommonEvents
 {
+
+    private static final List<String> IGNORE_ITEMS;
     private static final List<String> IGNORE_SOUNDS;
     private static final List<String> IGNORE_ENTITIES;
 
     static
     {
         ImmutableList.Builder<String> builder = ImmutableList.builder();
+        builder.add("body");
+        IGNORE_ITEMS = builder.build();
+
+        builder = ImmutableList.builder();
         builder.add("idle");
         builder.add("driving");
         IGNORE_SOUNDS = builder.build();
 
         builder = ImmutableList.builder();
+        builder.add("vehicle_atv");
         builder.add("couch");
         IGNORE_ENTITIES = builder.build();
     }
 
     @SubscribeEvent
-    public void onMissingMap(RegistryEvent.MissingMappings<SoundEvent> event)
+    public void onMissingItem(RegistryEvent.MissingMappings<Item> event)
+    {
+        for(RegistryEvent.MissingMappings.Mapping<Item> missing : event.getMappings())
+        {
+            if(missing.key.getResourceDomain().equals(Reference.MOD_ID) && IGNORE_ITEMS.contains(missing.key.getResourcePath()))
+            {
+                missing.ignore();
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onMissingSound(RegistryEvent.MissingMappings<SoundEvent> event)
     {
         for(RegistryEvent.MissingMappings.Mapping<SoundEvent> missing : event.getMappings())
         {
