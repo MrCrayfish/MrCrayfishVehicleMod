8037329765f55aec66f30dfd8b33a9491cec8faf
MrCrayfish <mrcrayfish@hotmail.com>
2018-05-16 21:24:29 +0930

    Added tool tip to Traffic Cone

--- src/main/java/com/mrcrayfish/vehicle/item/ItemTrafficCone.java
+++ src/main/java/com/mrcrayfish/vehicle/item/ItemTrafficCone.java
@@ -1,11 +1,17 @@
 package com.mrcrayfish.vehicle.item;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * Author: MrCrayfish
@@ -21,5 +27,19 @@ public class ItemTrafficCone extends ItemBlock
     public EntityEquipmentSlot getEquipmentSlot(ItemStack stack)
     {
         return EntityEquipmentSlot.HEAD;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        if(GuiScreen.isShiftKeyDown())
+        {
+            String info = "Use this cone to block vehicles with it's extra collision height. It can also be used as a hat!";
+            tooltip.addAll(Minecraft.getMinecraft().fontRenderer.listFormattedStringToWidth(info, 150));
+        }
+        else
+        {
+            tooltip.add(TextFormatting.YELLOW + "Hold SHIFT for Info");
+        }
     }
 }
