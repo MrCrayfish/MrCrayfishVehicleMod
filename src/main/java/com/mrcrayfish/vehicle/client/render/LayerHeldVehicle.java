40ba8f934f49b96842fcd44b0d73f677ba0026de
MrCrayfish <mrcrayfish@hotmail.com>
2018-06-03 00:13:44 +0930

    Fixed y offset to match when sneaking

--- src/main/java/com/mrcrayfish/vehicle/client/render/LayerHeldVehicle.java
+++ src/main/java/com/mrcrayfish/vehicle/client/render/LayerHeldVehicle.java
@@ -55,12 +55,11 @@ public class LayerHeldVehicle implements LayerRenderer<AbstractClientPlayer>
                         float width = cachedEntity.width / 2;
                         GlStateManager.translate(0F, 1F - 1F * counter.getProgress(partialTicks), -0.5F * Math.sin(Math.PI * counter.getProgress(partialTicks)) - width * (1.0F - counter.getProgress(partialTicks)));
                     }
-
                     Vec3d heldOffset = cachedEntity.getHeldOffset();
                     GlStateManager.translate(heldOffset.x * 0.0625D, heldOffset.y * 0.0625D, heldOffset.z * 0.0625D);
                     GlStateManager.rotate(180F, 1, 0, 0);
                     GlStateManager.rotate(-90F, 0, 1, 0);
-                    GlStateManager.translate(0F, player.isSneaking() ? 0.22F : 0.5F, 0F);
+                    GlStateManager.translate(0F, player.isSneaking() ? 0.3F : 0.5F, 0F);
                     Render<Entity> render = Minecraft.getMinecraft().getRenderManager().getEntityClassRenderObject(cachedClass);
                     render.doRender(cachedEntity, 0.0D, 0.0D, 0.0D, 0F, 0F);
                 }
