dc59ee4559c9e346dcdf5a592f18cdf4dd6d402e
MrCrayfish <mrcrayfish@hotmail.com>
2018-07-25 23:42:52 +0930

    Smart car can now tow a trailer

--- src/main/java/com/mrcrayfish/vehicle/entity/vehicle/EntitySmartCar.java
+++ src/main/java/com/mrcrayfish/vehicle/entity/vehicle/EntitySmartCar.java
@@ -29,6 +29,7 @@ public class EntitySmartCar extends EntityLandVehicle implements IEntityRaytrace
         this.setTurnSensitivity(12);
         this.setSize(1.85F, 1.15F);
         this.setHeldOffset(new Vec3d(3D, 1D, 0D));
+        this.setTowBarPosition(new Vec3d(0D, 0D, -1.5D));
         this.stepHeight = 1F;
     }
 
@@ -80,6 +81,12 @@ public class EntitySmartCar extends EntityLandVehicle implements IEntityRaytrace
 
     @Override
     public boolean canBeColored()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean canTowTrailer()
     {
         return true;
     }
