9621625d85e50922d5470f13c7b616891277c1f6
MrCrayfish <mrcrayfish@hotmail.com>
2018-07-31 01:00:40 +0930

    The long awaited fix for crash when placing vehicles

--- src/main/java/com/mrcrayfish/vehicle/Reference.java
+++ src/main/java/com/mrcrayfish/vehicle/Reference.java
@@ -9,7 +9,7 @@ public class Reference
     public static final String MOD_NAME = "Vehicle Mod";
     public static final String MOD_VERSION = "0.18.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
-    public static final String MOD_DEPENDS = "required-after:obfuscate@[0.2.3,);after:cfm@[4.1.5,)";
+    public static final String MOD_DEPENDS = "required-after:obfuscate@[0.2.5,);after:cfm@[4.1.5,)";
 
     public static final String PROXY_CLIENT = "com.mrcrayfish.vehicle.proxy.ClientProxy";
     public static final String PROXY_SERVER = "com.mrcrayfish.vehicle.proxy.ServerProxy";
