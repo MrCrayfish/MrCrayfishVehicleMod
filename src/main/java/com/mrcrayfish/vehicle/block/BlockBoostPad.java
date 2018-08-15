9c6dae0aa4eae8536565e57db0dd706aa595c147
MrCrayfish <mrcrayfish@hotmail.com>
2018-08-15 15:52:26 +0930

    Added information to Boost Pad and Ramp

--- src/main/java/com/mrcrayfish/vehicle/block/BlockBoostPad.java
+++ src/main/java/com/mrcrayfish/vehicle/block/BlockBoostPad.java
@@ -7,15 +7,21 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * Author: MrCrayfish
@@ -31,6 +37,20 @@ public class BlockBoostPad extends BlockRotatedObject
     {
         super(Material.ROCK, "boost_pad");
         this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(LEFT, false).withProperty(RIGHT, false));
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced)
+    {
+        if(GuiScreen.isShiftKeyDown())
+        {
+            String info = "Drive on this Boost Pad to give your vehicle a speed boost!";
+            tooltip.addAll(Minecraft.getMinecraft().fontRenderer.listFormattedStringToWidth(info, 150));
+        }
+        else
+        {
+            tooltip.add(TextFormatting.YELLOW + "Hold SHIFT for Info");
+        }
     }
 
     @Override
--- src/main/java/com/mrcrayfish/vehicle/block/BlockBoostRamp.java
+++ src/main/java/com/mrcrayfish/vehicle/block/BlockBoostRamp.java
@@ -9,11 +9,16 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -53,6 +58,20 @@ public class BlockBoostRamp extends BlockRotatedObject
     public BlockBoostRamp()
     {
         super(Material.ROCK, "boost_ramp");
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced)
+    {
+        if(GuiScreen.isShiftKeyDown())
+        {
+            String info = "Drive on this Boost Ramp to give your vehicle a speed boost and send it flying into the air!";
+            tooltip.addAll(Minecraft.getMinecraft().fontRenderer.listFormattedStringToWidth(info, 150));
+        }
+        else
+        {
+            tooltip.add(TextFormatting.YELLOW + "Hold SHIFT for Info");
+        }
     }
 
     @Override
