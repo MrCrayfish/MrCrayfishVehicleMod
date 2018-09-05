609590283dfeb6e7b97a29c272755194d5f9c877
Phylogeny <molecularphylo@gmail.com>
2018-09-04 20:27:32 -0700

    Allowed fluid pumps to losslessly/evenly distribute fluid to all connections at a target rate of 10mB per connection per tick. (#38)

--- src/main/java/com/mrcrayfish/vehicle/tileentity/TileEntityFluidPump.java
+++ src/main/java/com/mrcrayfish/vehicle/tileentity/TileEntityFluidPump.java
@@ -1,5 +1,9 @@
 package com.mrcrayfish.vehicle.tileentity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 import com.mrcrayfish.vehicle.block.BlockFluidPipe;
 import com.mrcrayfish.vehicle.block.BlockFluidPump;
 import com.mrcrayfish.vehicle.util.FluidUtils;
@@ -44,20 +48,66 @@ public class TileEntityFluidPump extends TileFluidHandler implements ITickable
             }
         }
 
+        // Collect connections
         state = state.getActualState(world, pos);
+        List<IFluidHandler> fluidHandlers = new ArrayList<>();
         for(EnumFacing face : EnumFacing.VALUES)
         {
             if(state.getValue(BlockFluidPump.CONNECTED_PIPES[face.getIndex()]))
             {
                 tileEntity = world.getTileEntity(pos.offset(face));
-                if(tileEntity != null && tileEntity.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, face))
+                if(tileEntity != null)
                 {
                     IFluidHandler handler = tileEntity.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, face);
                     if(handler != null)
                     {
-                        FluidUtils.transferFluid(tank, handler, TRANSFER_AMOUNT);
+                        fluidHandlers.add(handler);
                     }
                 }
+            }
+        }
+
+        // Return if no connections, or return and transfer full amount if one connection
+        int count = fluidHandlers.size();
+        if(count <= 1)
+        {
+            if (count == 1)
+            {
+                FluidUtils.transferFluid(tank, fluidHandlers.get(0), TRANSFER_AMOUNT);
+            }
+            return;
+        }
+
+        // Evenly distribute truncated proportion to all connections
+        int remainder = Math.min(tank.getFluidAmount(), TRANSFER_AMOUNT * count);
+        int amount = remainder / count;
+        int filled;
+        if(amount > 0)
+        {
+            ListIterator<IFluidHandler> iterator = fluidHandlers.listIterator();
+            while(iterator.hasNext())
+            {
+                if(FluidUtils.transferFluid(tank, iterator.next(), amount) < amount)
+                {
+                    iterator.remove();
+                }
+            }
+        }
+
+        // Randomly distribute to the remaining non-full connections the proportion that would otherwise be lost in the above truncation
+        remainder %= count;
+        if(fluidHandlers.size() == 1)
+        {
+            FluidUtils.transferFluid(tank, fluidHandlers.get(0), remainder);
+        }
+        for(int i = 0; i < remainder && !fluidHandlers.isEmpty(); i++)
+        {
+            int index = world.rand.nextInt(fluidHandlers.size());
+            filled = FluidUtils.transferFluid(tank, fluidHandlers.get(index), 1);
+            remainder -= filled;
+            if(filled == 0)
+            {
+                fluidHandlers.remove(index);
             }
         }
     }
