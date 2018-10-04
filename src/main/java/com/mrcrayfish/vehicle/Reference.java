ddf10b263252c395db81cf31010c654770fccaf5
MrCrayfish <mrcrayfish@hotmail.com>
2018-10-04 09:50:20 +0930

    Update version

--- src/main/java/com/mrcrayfish/vehicle/Reference.java
+++ src/main/java/com/mrcrayfish/vehicle/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 {
     public static final String MOD_ID = "vehicle";
     public static final String MOD_NAME = "MrCrayfish's Vehicle Mod";
-    public static final String MOD_VERSION = "0.25.0";
+    public static final String MOD_VERSION = "0.26.0";
     public static final String MOD_COMPATIBILITY = "[1.12.2]";
     public static final String MOD_DEPENDS = "required-after:obfuscate@[0.2.5,);after:cfm@[4.1.5,);required-after:forge@[14.23.4.2705,)";
 
