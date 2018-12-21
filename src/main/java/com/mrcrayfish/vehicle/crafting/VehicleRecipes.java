4acb14c401d7523bf1b918f4879f7e0578b35cb6
MrCrayfish <mrcrayfish@hotmail.com>
2018-12-21 18:02:47 +1030

    Added recipe for Fertilizer Trailer

--- src/main/java/com/mrcrayfish/vehicle/crafting/VehicleRecipes.java
+++ src/main/java/com/mrcrayfish/vehicle/crafting/VehicleRecipes.java
@@ -2,6 +2,7 @@ package com.mrcrayfish.vehicle.crafting;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.mrcrayfish.vehicle.entity.trailer.EntityFertilizerTrailer;
 import com.mrcrayfish.vehicle.entity.trailer.EntitySeederTrailer;
 import com.mrcrayfish.vehicle.entity.trailer.EntityStorageTrailer;
 import com.mrcrayfish.vehicle.entity.trailer.EntityVehicleTrailer;
@@ -171,10 +172,16 @@ public class VehicleRecipes
         mapBuilder.put(EntityStorageTrailer.class, builder.build());
 
         builder = new Builder();
-        builder.addMaterial(new ItemStack(Items.IRON_INGOT, 36));
+        builder.addMaterial(new ItemStack(Items.IRON_INGOT, 42));
         builder.addMaterial(new ItemStack(ModItems.WHEEL, 2));
         builder.addMaterial(new ItemStack(ModItems.PANEL, 8));
         mapBuilder.put(EntitySeederTrailer.class, builder.build());
+
+        builder = new Builder();
+        builder.addMaterial(new ItemStack(Items.IRON_INGOT, 36));
+        builder.addMaterial(new ItemStack(ModItems.WHEEL, 2));
+        builder.addMaterial(new ItemStack(ModItems.PANEL, 8));
+        mapBuilder.put(EntityFertilizerTrailer.class, builder.build());
 
         if(Loader.isModLoaded("cfm"))
         {
