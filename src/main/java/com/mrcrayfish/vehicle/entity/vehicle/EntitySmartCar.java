eb7028801b38d69e4adfed0141c4dbac5c77c20c
MrCrayfish <mrcrayfish@hotmail.com>
2019-02-13 18:51:46 +1030

    Fixed Smart Car tow bar position

--- src/main/java/com/mrcrayfish/vehicle/entity/vehicle/EntitySmartCar.java
+++ src/main/java/com/mrcrayfish/vehicle/entity/vehicle/EntitySmartCar.java
@@ -23,7 +23,7 @@ public class EntitySmartCar extends EntityLandVehicle implements IEntityRaytrace
     public static final PartPosition BODY_POSITION = new PartPosition(0, 0, 0.2, 0, 0, 0, 1.25);
     public static final PartPosition FUEL_PORT_POSITION = new PartPosition(-9.25, 8.7, -12.3, 0, -90, 0, 0.25);
     private static final Vec3d HELD_OFFSET_VEC = new Vec3d(3.0D, 1.0D, 0.0D);
-    private static final Vec3d TOW_BAR_VEC = new Vec3d(0.0D, 0.0D, -21.0D);
+    private static final Vec3d TOW_BAR_VEC = new Vec3d(0.0D, 0.0D, -24.5D);
 
     /**
      * ItemStack instances used for rendering
