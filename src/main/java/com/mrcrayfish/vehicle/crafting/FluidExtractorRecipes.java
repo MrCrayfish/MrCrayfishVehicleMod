bd5b4dd5a7e8daf75d585470f964aa74246ca325
MrCrayfish <mrcrayfish@hotmail.com>
2019-02-18 18:49:29 +1030

    Increased extraction amount

--- src/main/java/com/mrcrayfish/vehicle/crafting/FluidExtractorRecipes.java
+++ src/main/java/com/mrcrayfish/vehicle/crafting/FluidExtractorRecipes.java
@@ -25,8 +25,8 @@ public class FluidExtractorRecipes
     private FluidExtractorRecipes()
     {
         ImmutableMap.Builder<ItemStack, FluidExtract> builder = new ImmutableMap.Builder<>();
-        builder.put(new ItemStack(Items.ENDER_PEARL), new FluidExtract(ModFluids.ENDER_SAP, 100));
-        builder.put(new ItemStack(Items.BLAZE_ROD), new FluidExtract(ModFluids.BLAZE_JUICE, 80));
+        builder.put(new ItemStack(Items.ENDER_PEARL), new FluidExtract(ModFluids.ENDER_SAP, 500));
+        builder.put(new ItemStack(Items.BLAZE_ROD), new FluidExtract(ModFluids.BLAZE_JUICE, 350));
         extractingMap = builder.build();
     }
 
