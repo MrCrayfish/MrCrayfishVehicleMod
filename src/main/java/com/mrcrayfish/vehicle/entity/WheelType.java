cf7ab3c28be60ea2c4ead7d7b82d1ea35f70fa80
MrCrayfish <mrcrayfish@hotmail.com>
2019-02-27 15:52:45 +1030

    Readded Dune Buggy "plastic" wheels

--- src/main/java/com/mrcrayfish/vehicle/entity/WheelType.java
+++ src/main/java/com/mrcrayfish/vehicle/entity/WheelType.java
@@ -5,12 +5,10 @@ package com.mrcrayfish.vehicle.entity;
  */
 public enum WheelType
 {
-    NONE(1.0F, 0.75F, 0.25F),
-    STANDARD(1.0F, 0.8F, 0.25F),
-    SPORTS(1.1F, 0.7F, 0.25F),
-    RACING(1.25F, 0.5F, 0.15F),
-    DIRT(0.9F, 1.0F, 0.5F),
-    OFF_ROAD(0.8F, 1.1F, 0.50F),
+    STANDARD(0.9F, 0.8F, 0.25F),
+    SPORTS(1.0F, 0.7F, 0.25F),
+    RACING(1.1F, 0.5F, 0.15F),
+    OFF_ROAD(0.8F, 1.0F, 0.50F),
     SNOW(0.75F, 0.9F, 1.0F),
     ALL_TERRAIN(0.85F, 0.85F, 0.85F),
     WATER(0.25F, 0.25F, 0.25F)
@@ -23,7 +21,8 @@ public enum WheelType
                 vehicle.motionY += 0.1;
             }
         }
-    };
+    },
+    PLASTIC(0.5F, 0.5F, 0.5F);
 
     float roadMultiplier;
     float dirtMultiplier;
@@ -57,7 +56,7 @@ public enum WheelType
     {
         if(index < 0 || index >= values().length)
         {
-            return NONE;
+            return STANDARD;
         }
         return WheelType.values()[index];
     }
