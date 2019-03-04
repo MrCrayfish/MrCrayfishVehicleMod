c9ed4cbe6e73229a5c46ca8415aecc5ad316f27c
MrCrayfish <mrcrayfish@hotmail.com>
2019-03-05 03:46:53 +1030

    Added support to control player movement with controller

--- src/main/java/com/mrcrayfish/vehicle/client/ControllerEvents.java
+++ src/main/java/com/mrcrayfish/vehicle/client/ControllerEvents.java
@@ -6,7 +6,9 @@ import com.mrcrayfish.vehicle.entity.EntityVehicle;
 import com.mrcrayfish.vehicle.network.PacketHandler;
 import com.mrcrayfish.vehicle.network.message.MessageHitchTrailer;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.client.event.InputUpdateEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -38,13 +40,17 @@ public class ControllerEvents
         if(!VehicleConfig.CLIENT.experimental.controllerSupport || controller == null)
             return;
 
+        if(event.phase == TickEvent.Phase.END)
+            return;
+
+        /* Handles rotating the yaw of player */
         if(controller.getZAxisValue() != 0.0F)
         {
             EntityPlayer player = Minecraft.getMinecraft().player;
-            player.rotationYaw += 1.0F * (controller.getZAxisValue() > 0.0F ? 1 : -1) * Math.abs(controller.getZAxisValue());
+            player.rotationYaw += 2.0F * (controller.getZAxisValue() > 0.0F ? 1 : -1) * Math.abs(controller.getZAxisValue());
         }
 
-        /* Handles rotating the pitch of player*/
+        /* Handles rotating the pitch of player */
         if(controller.getRZAxisValue() != 0.0F)
         {
             EntityPlayer player = Minecraft.getMinecraft().player;
@@ -78,6 +84,33 @@ public class ControllerEvents
                         }
                     }
                 }
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onInputUpdate(InputUpdateEvent event)
+    {
+        if(!VehicleConfig.CLIENT.experimental.controllerSupport || controller == null)
+            return;
+
+        EntityPlayer player = Minecraft.getMinecraft().player;
+        if(Minecraft.getMinecraft().currentScreen == null && !(player.getRidingEntity() instanceof EntityVehicle))
+        {
+            if(controller.getYAxisValue() != 0.0F)
+            {
+                int dir = controller.getYAxisValue() > 0.0F ? -1 : 1;
+                event.getMovementInput().forwardKeyDown = dir > 0;
+                event.getMovementInput().backKeyDown = dir < 0;
+                event.getMovementInput().moveForward = dir * Math.abs(controller.getYAxisValue());
+            }
+
+            if(controller.getXAxisValue() != 0.0F)
+            {
+                int dir = controller.getXAxisValue() > 0.0F ? -1 : 1;
+                event.getMovementInput().rightKeyDown = dir < 0;
+                event.getMovementInput().leftKeyDown = dir > 0;
+                event.getMovementInput().moveStrafe += dir;
             }
         }
     }
