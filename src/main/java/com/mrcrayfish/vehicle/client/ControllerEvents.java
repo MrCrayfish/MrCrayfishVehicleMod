63ff1b9be97625b365652282bbaccb29c79ad6de
MrCrayfish <mrcrayfish@hotmail.com>
2019-03-05 03:13:39 +1030

    Added support to hitch trailers with controllers

--- src/main/java/com/mrcrayfish/vehicle/client/ControllerEvents.java
+++ src/main/java/com/mrcrayfish/vehicle/client/ControllerEvents.java
@@ -2,9 +2,15 @@ package com.mrcrayfish.vehicle.client;
 
 import com.mrcrayfish.vehicle.Reference;
 import com.mrcrayfish.vehicle.VehicleConfig;
+import com.mrcrayfish.vehicle.entity.EntityVehicle;
+import com.mrcrayfish.vehicle.network.PacketHandler;
+import com.mrcrayfish.vehicle.network.message.MessageHitchTrailer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.lwjgl.input.Controller;
 import org.lwjgl.input.Controllers;
 
@@ -21,6 +27,34 @@ public class ControllerEvents
     {
         if(!VehicleConfig.CLIENT.experimental.controllerSupport || controller == null)
             return;
+
+        /* Updates the values in the controller */
         controller.poll();
+    }
+
+    @SubscribeEvent
+    public static void onTick(TickEvent.RenderTickEvent event)
+    {
+        if(!VehicleConfig.CLIENT.experimental.controllerSupport || controller == null)
+            return;
+
+        while(Controllers.next())
+        {
+            if(Controllers.getEventSource().equals(controller))
+            {
+                if(Controllers.getEventButtonState() && Controllers.getEventControlIndex() == 11)
+                {
+                    EntityPlayer player = Minecraft.getMinecraft().player;
+                    if(Minecraft.getMinecraft().currentScreen == null && player.getRidingEntity() instanceof EntityVehicle)
+                    {
+                        EntityVehicle vehicle = (EntityVehicle) player.getRidingEntity();
+                        if(vehicle.canTowTrailer())
+                        {
+                            PacketHandler.INSTANCE.sendToServer(new MessageHitchTrailer(vehicle.getTrailer() == null));
+                        }
+                    }
+                }
+            }
+        }
     }
 }
