5e3f80829956c4a392c4474351c35c71f8920cb3
MrCrayfish <mrcrayfish@hotmail.com>
2020-01-03 01:20:37 +1030

    Fixed workstation selection and collision box

--- src/main/java/com/mrcrayfish/vehicle/block/BlockWorkstation.java
+++ src/main/java/com/mrcrayfish/vehicle/block/BlockWorkstation.java
@@ -2,6 +2,7 @@ package com.mrcrayfish.vehicle.block;
 
 import com.mrcrayfish.vehicle.tileentity.WorkstationTileEntity;
 import com.mrcrayfish.vehicle.util.Names;
+import com.mrcrayfish.vehicle.util.VoxelShapeHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
@@ -9,22 +10,58 @@ import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Author: MrCrayfish
  */
 public class BlockWorkstation extends BlockRotatedObject
 {
+    private final Map<BlockState, VoxelShape> SHAPES = new HashMap<>();
+
     public BlockWorkstation()
     {
         super(Names.Block.WORKSTATION, Block.Properties.create(Material.IRON).hardnessAndResistance(1.0F));
+    }
+
+    private VoxelShape getShape(BlockState state)
+    {
+        if(SHAPES.containsKey(state))
+        {
+            return SHAPES.get(state);
+        }
+        Direction direction = state.get(DIRECTION);
+        List<VoxelShape> shapes = new ArrayList<>();
+        shapes.add(Block.makeCuboidShape(0, 1, 0, 16, 16, 16));
+        shapes.add(VoxelShapeHelper.getRotatedShapes(VoxelShapeHelper.rotate(Block.makeCuboidShape(0, 16, 0, 16, 18, 2), Direction.SOUTH))[direction.getHorizontalIndex()]);
+        VoxelShape shape = VoxelShapeHelper.combineAll(shapes);
+        SHAPES.put(state, shape);
+        return shape;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader reader, BlockPos pos, ISelectionContext context)
+    {
+        return this.getShape(state);
+    }
+
+    @Override
+    public VoxelShape getRenderShape(BlockState state, IBlockReader reader, BlockPos pos)
+    {
+        return this.getShape(state);
     }
 
     @Override
