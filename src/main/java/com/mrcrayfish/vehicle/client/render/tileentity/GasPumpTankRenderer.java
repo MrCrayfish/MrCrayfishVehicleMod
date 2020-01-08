d6e09e5a43d02cee68a89086ced7e802ec58e7aa
MrCrayfish <mrcrayfish@hotmail.com>
2020-01-08 14:52:04 +1030

    Fixed fluid rendering on gas pump tank

--- src/main/java/com/mrcrayfish/vehicle/client/render/tileentity/GasPumpTankRenderer.java
+++ src/main/java/com/mrcrayfish/vehicle/client/render/tileentity/GasPumpTankRenderer.java
@@ -2,15 +2,14 @@ package com.mrcrayfish.vehicle.client.render.tileentity;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.mrcrayfish.vehicle.block.BlockRotatedObject;
 import com.mrcrayfish.vehicle.client.render.Axis;
 import com.mrcrayfish.vehicle.init.ModBlocks;
 import com.mrcrayfish.vehicle.tileentity.GasPumpTankTileEntity;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
@@ -31,7 +30,7 @@ public class GasPumpTankRenderer extends TileEntityRenderer<GasPumpTankTileEntit
     }
 
     @Override
-    public void func_225616_a_(GasPumpTankTileEntity gasPump, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer iRenderTypeBuffer, int light, int overlay)
+    public void func_225616_a_(GasPumpTankTileEntity gasPump, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int light, int overlay)
     {
         BlockState state = gasPump.getWorld().getBlockState(gasPump.getPos());
         if(state.getBlock() != ModBlocks.GAS_PUMP)
@@ -44,56 +43,43 @@ public class GasPumpTankRenderer extends TileEntityRenderer<GasPumpTankTileEntit
         matrixStack.func_227863_a_(Axis.POSITIVE_Y.func_229187_a_(facing.getHorizontalIndex() * -90F - 90F));
         matrixStack.func_227861_a_(-0.5, -0.5, -0.5);
         float height = 11.0F * (gasPump.getFluidTank().getFluidAmount() / (float) gasPump.getFluidTank().getCapacity());
-        if(height > 0) drawFluid(gasPump, matrixStack, 3.01 * 0.0625, 3 * 0.0625, 4 * 0.0625, (10 - 0.02F) * 0.0625F, height * 0.0625F, (8 - 0.02F) * 0.0625F, light);
+        if(height > 0) drawFluid(gasPump, matrixStack, renderTypeBuffer, 3.01F * 0.0625F, 3F * 0.0625F, 4F * 0.0625F, (10 - 0.02F) * 0.0625F, height * 0.0625F, (8 - 0.02F) * 0.0625F, light);
 
         matrixStack.func_227865_b_();
     }
 
-    private void drawFluid(GasPumpTankTileEntity te, MatrixStack matrixStack, double x, double y, double z, float width, float height, float depth, int light)
+    private void drawFluid(GasPumpTankTileEntity te, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, float x, float y, float z, float width, float height, float depth, int light)
     {
         if(te.getFluidTank().isEmpty())
             return;
 
-        Minecraft.getInstance().getTextureManager().bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
         TextureAtlasSprite sprite = ForgeHooksClient.getFluidSprites(te.getWorld(), te.getPos(), te.getFluidTank().getFluid().getFluid().getDefaultState())[0];
         float minU = sprite.getMinU();
         float maxU = Math.min(minU + (sprite.getMaxU() - minU) * depth, sprite.getMaxU());
         float minV = sprite.getMinV();
         float maxV = Math.min(minV + (sprite.getMaxV() - minV) * height, sprite.getMaxV());
 
-        RenderSystem.pushMatrix();
-        RenderSystem.pushLightingAttributes();
-        RenderSystem.multMatrix(matrixStack.func_227866_c_().func_227870_a_());
-        RenderSystem.enableBlend();
-        RenderSystem.enableDepthTest();
-
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder buffer = tessellator.getBuffer();
+        IVertexBuilder buffer = renderTypeBuffer.getBuffer(RenderType.func_228645_f_());
+        Matrix4f matrix = matrixStack.func_227866_c_().func_227870_a_();
 
         //back side
-        buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
-        buffer.func_225582_a_(x + width, y, z + depth).func_225583_a_(maxU, minV).func_227886_a_(light).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).endVertex();
-        buffer.func_225582_a_(x + width, y, z).func_225583_a_(minU, minV).func_227886_a_(light).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).endVertex();
-        buffer.func_225582_a_(x + width, y + height, z).func_225583_a_(minU, maxV).func_227886_a_(light).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).endVertex();
-        buffer.func_225582_a_(x + width, y + height, z + depth).func_225583_a_(maxU, maxV).func_227886_a_(light).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).endVertex();
-        tessellator.draw();
+        buffer.func_227888_a_(matrix, x + width, y, z + depth).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).func_225583_a_(maxU, minV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x + width, y, z).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).func_225583_a_(minU, minV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x + width, y + height, z).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).func_225583_a_(minU, maxV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x + width, y + height, z + depth).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).func_225583_a_(maxU, maxV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
 
         //front side
-        buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
-        buffer.func_225582_a_(x, y, z + depth).func_225583_a_(maxU, minV).func_227886_a_(light).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).endVertex();
-        buffer.func_225582_a_(x, y, z).func_225583_a_(minU, minV).func_227886_a_(light).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).endVertex();
-        buffer.func_225582_a_(x, y + height, z).func_225583_a_(minU, maxV).func_227886_a_(light).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).endVertex();
-        buffer.func_225582_a_(x, y + height, z + depth).func_225583_a_(maxU, maxV).func_227886_a_(light).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).endVertex();
-        tessellator.draw();
+        buffer.func_227888_a_(matrix, x, y, z).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).func_225583_a_(minU, minV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x, y, z + depth).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).func_225583_a_(maxU, minV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x, y + height, z + depth).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).func_225583_a_(maxU, maxV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x, y + height, z).func_227885_a_(0.85F, 0.85F, 0.85F, 1.0F).func_225583_a_(minU, maxV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
 
         maxV = Math.min(minV + (sprite.getMaxV() - minV) * width, sprite.getMaxV());
 
         //top
-        buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
-        buffer.func_225582_a_(x, y + height, z).func_225583_a_(maxU, minV).func_227886_a_(light).func_227885_a_(1.0F, 1.0F, 1.0F, 1.0F).endVertex();
-        buffer.func_225582_a_(x, y + height, z + depth).func_225583_a_(minU, minV).func_227886_a_(light).func_227885_a_(1.0F, 1.0F, 1.0F, 1.0F).endVertex();
-        buffer.func_225582_a_(x + width, y + height, z + depth).func_225583_a_(minU, maxV).func_227886_a_(light).func_227885_a_(1.0F, 1.0F, 1.0F, 1.0F).endVertex();
-        buffer.func_225582_a_(x + width, y + height, z).func_225583_a_(maxU, maxV).func_227886_a_(light).func_227885_a_(1.0F, 1.0F, 1.0F, 1.0F).endVertex();
-        tessellator.draw();
+        buffer.func_227888_a_(matrix, x, y + height, z).func_227885_a_(1.0F, 1.0F, 1.0F, 1.0F).func_225583_a_(maxU, minV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x, y + height, z + depth).func_227885_a_(1.0F, 1.0F, 1.0F, 1.0F).func_225583_a_(minU, minV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x + width, y + height, z + depth).func_227885_a_(1.0F, 1.0F, 1.0F, 1.0F).func_225583_a_(minU, maxV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
+        buffer.func_227888_a_(matrix, x + width, y + height, z).func_227885_a_(1.0F, 1.0F, 1.0F, 1.0F).func_225583_a_(maxU, maxV).func_227886_a_(light).func_225584_a_(0.0F, 1.0F, 0.0F).endVertex();
     }
 }
