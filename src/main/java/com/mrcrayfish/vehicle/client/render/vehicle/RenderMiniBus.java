8661180cdbe7bbab9f1949a530217b63fbdfe3bb
MrCrayfish <mrcrayfish@hotmail.com>
2020-03-01 16:55:36 +1030

    Added steering wheel to mini bus

--- src/main/java/com/mrcrayfish/vehicle/client/render/vehicle/RenderMiniBus.java
+++ src/main/java/com/mrcrayfish/vehicle/client/render/vehicle/RenderMiniBus.java
@@ -4,8 +4,18 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mrcrayfish.vehicle.client.ISpecialModel;
 import com.mrcrayfish.vehicle.client.SpecialModels;
 import com.mrcrayfish.vehicle.client.render.AbstractRenderVehicle;
+import com.mrcrayfish.vehicle.client.render.Axis;
 import com.mrcrayfish.vehicle.entity.vehicle.MiniBusEntity;
+import com.mrcrayfish.vehicle.util.RenderUtil;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.entity.model.PlayerModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.List;
 
 /**
  * Author: MrCrayfish
@@ -22,5 +32,38 @@ public class RenderMiniBus extends AbstractRenderVehicle<MiniBusEntity>
     public void render(MiniBusEntity entity, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, float partialTicks, int light)
     {
         this.renderDamagedPart(entity, SpecialModels.MINI_BUS_BODY.getModel(), matrixStack, renderTypeBuffer, light);
+
+        matrixStack.push();
+
+        // Positions the steering wheel in the correct position
+        matrixStack.translate(-0.2825, 0.225, 1.0625);
+        matrixStack.rotate(Axis.POSITIVE_X.func_229187_a_(-67.5F));
+        matrixStack.translate(0, -0.02, 0);
+        matrixStack.scale(0.75F, 0.75F, 0.75F);
+
+        // Rotates the steering wheel based on the wheel angle
+        float wheelAngle = entity.prevRenderWheelAngle + (entity.renderWheelAngle - entity.prevRenderWheelAngle) * partialTicks;
+        float wheelAngleNormal = wheelAngle / 45F;
+        float turnRotation = wheelAngleNormal * 25F;
+        matrixStack.rotate(Axis.POSITIVE_Y.func_229187_a_(turnRotation));
+
+        RenderUtil.renderColoredModel(SpecialModels.GO_KART_STEERING_WHEEL.getModel(), ItemCameraTransforms.TransformType.NONE, false, matrixStack, renderTypeBuffer, -1, light, OverlayTexture.DEFAULT_LIGHT);
+
+        matrixStack.pop();
+    }
+
+    @Override
+    public void applyPlayerModel(MiniBusEntity entity, PlayerEntity player, PlayerModel<AbstractClientPlayerEntity> model, float partialTicks)
+    {
+        if(entity.getControllingPassenger() == player)
+        {
+            float wheelAngle = entity.prevRenderWheelAngle + (entity.renderWheelAngle - entity.prevRenderWheelAngle) * partialTicks;
+            float wheelAngleNormal = wheelAngle / 45F;
+            float turnRotation = wheelAngleNormal * 6F;
+            model.bipedRightArm.rotateAngleX = (float) Math.toRadians(-65F - turnRotation);
+            model.bipedRightArm.rotateAngleY = (float) Math.toRadians(-7F);
+            model.bipedLeftArm.rotateAngleX = (float) Math.toRadians(-65F + turnRotation);
+            model.bipedLeftArm.rotateAngleY = (float) Math.toRadians(7F);
+        }
     }
 }
