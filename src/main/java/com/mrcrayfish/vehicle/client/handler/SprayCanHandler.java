4918abb03b9f394777f388009490e4b93dc49b09
MrCrayfish <mrcrayfish@hotmail.com>
2021-07-21 02:17:09 +0930

    🎨 New spray can model

--- src/main/java/com/mrcrayfish/vehicle/client/handler/SprayCanHandler.java
+++ src/main/java/com/mrcrayfish/vehicle/client/handler/SprayCanHandler.java
@@ -1,6 +1,5 @@
 package com.mrcrayfish.vehicle.client.handler;
 
-import com.mrcrayfish.obfuscate.client.event.RenderItemEvent;
 import com.mrcrayfish.vehicle.init.ModSounds;
 import com.mrcrayfish.vehicle.item.SprayCanItem;
 import net.minecraft.client.Minecraft;
@@ -10,7 +9,6 @@ import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.HandSide;
-import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -43,30 +41,6 @@ public class SprayCanHandler
         SprayCanItem sprayCan = (SprayCanItem) player.inventory.getSelected().getItem();
         float pitch = 0.85F + 0.15F * sprayCan.getRemainingSprays(player.inventory.getSelected());
         Minecraft.getInstance().getSoundManager().play(SimpleSound.forUI(ModSounds.ITEM_SPRAY_CAN_SHAKE.get(), pitch, 0.75F));
-    }
-
-    @SubscribeEvent
-    public void onRenderHand(RenderHandEvent event)
-    {
-        if(!event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof SprayCanItem)
-        {
-            //ItemStack stack = event.getItemStack().copy(); //TODO fix the spray can item render
-            //stack.setItemDamage(1);
-            //Minecraft.getMinecraft().getItemRenderer().renderItemInFirstPerson(Minecraft.getMinecraft().player, event.getPartialTicks(), event.getInterpolatedPitch(), event.getHand(), event.getSwingProgress(), stack, event.getEquipProgress());
-            //event.setCanceled(true);
-        }
-    }
-
-    @SubscribeEvent
-    public void onRenderThirdPerson(RenderItemEvent.Held.Pre event)
-    {
-        if(!event.getItem().isEmpty() && event.getItem().getItem() instanceof SprayCanItem)
-        {
-            /*ItemStack stack = event.getItem().copy(); //TODO fix this spray can render
-            stack.setItemDamage(1);
-            Minecraft.getMinecraft().getItemRenderer().renderItemSide(event.getEntity(), stack, event.getTransformType(), event.getHandSide() == EnumHandSide.LEFT);
-            event.setCanceled(true);*/
-        }
     }
 
     /**
