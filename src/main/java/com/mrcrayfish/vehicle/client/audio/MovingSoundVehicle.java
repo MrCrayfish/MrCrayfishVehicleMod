04829aec54a89ec2e093f8bd01018f91e860fa42
MrCrayfish <mrcrayfish@hotmail.com>
2021-08-07 23:01:31 +0930

    📦 Updated moving vehicle sound to use same engine pitch as riding version

--- src/main/java/com/mrcrayfish/vehicle/client/audio/MovingSoundVehicle.java
+++ src/main/java/com/mrcrayfish/vehicle/client/audio/MovingSoundVehicle.java
@@ -5,6 +5,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.TickableSound;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -43,7 +44,7 @@ public class MovingSoundVehicle extends TickableSound
             this.x = (float) (vehicle.getX() + (localPlayer.getX() - vehicle.getX()) * 0.65);
             this.y = (float) (vehicle.getY() + (localPlayer.getY() - vehicle.getY()) * 0.65);
             this.z = (float) (vehicle.getZ() + (localPlayer.getZ() - vehicle.getZ()) * 0.65);
-            this.pitch = (float) (vehicle.getMinEnginePitch() + (vehicle.getMaxEnginePitch() - vehicle.getMinEnginePitch()) * Math.abs(vehicle.getSpeed()));
+            this.pitch = MathHelper.lerp(0.2F, this.pitch, vehicle.getEnginePitch());
         }
         else
         {
