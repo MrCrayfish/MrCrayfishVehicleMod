67cdf1ae508078f41d39bdb2aa3995b9eddd6ebd
MrCrayfish <mrcrayfish@hotmail.com>
2021-08-24 04:31:57 +0930

    ✨ Added support for Biodiesel from Immersive Engineering

--- src/main/java/com/mrcrayfish/vehicle/Config.java
+++ src/main/java/com/mrcrayfish/vehicle/Config.java
@@ -3,6 +3,7 @@ package com.mrcrayfish.vehicle;
 import net.minecraftforge.common.ForgeConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -97,7 +98,7 @@ public class Config
                 this.pickUpVehicles = builder.comment("Allows players to pick up vehicles by crouching and right clicking").translation(Reference.MOD_ID + ".config.server.pick_up_vehicles").define("pickUpVehicles", true);
                 this.fuelConsumptionFactor = builder.comment("Change the amount of fuel vehicles consumes by multiplying the consumption rate by this factor").translation(Reference.MOD_ID + ".config.server.fuel_consumption_modifier").defineInRange("fuelConsumptionModifier", 1.0, 0.0, Double.MAX_VALUE);
                 this.disabledVehicles = builder.comment("A list of vehicles that are prevented from being crafted in the workstation").defineList("disabledVehicles", Collections.emptyList(), o -> true);
-                this.validFuels = builder.comment("A list of fluids that can be used as fuel for vehicles").defineList("validFuels", Collections.singletonList("vehicle:fuelium"), o -> true);
+                this.validFuels = builder.comment("A list of fluids that can be used as fuel for vehicles").defineList("validFuels", Arrays.asList("vehicle:fuelium", "immersiveengineering:biodiesel"), o -> true);
                 builder.pop();
 
                 builder.comment("Configuration options for trailers").push("trailer");
