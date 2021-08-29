fac4ffc4414526058c592c818d0d13f578aba2e2
MrCrayfish <mrcrayfish@hotmail.com>
2021-08-30 00:52:15 +0930

    📦 Improved engine pitch of plane

--- src/main/java/com/mrcrayfish/vehicle/entity/PlaneEntity.java
+++ src/main/java/com/mrcrayfish/vehicle/entity/PlaneEntity.java
@@ -405,9 +405,7 @@ public abstract class PlaneEntity extends PoweredVehicleEntity
     @OnlyIn(Dist.CLIENT)
     protected void updateEngineSound()
     {
-        float normal = MathHelper.clamp(this.propellerSpeed / 200F, 0.0F, 1.25F) * 0.6F;
-        normal += (this.motion.scale(20).length() / this.getProperties().getEnginePower()) * 0.4F;
-        this.enginePitch = this.getMinEnginePitch() + (this.getMaxEnginePitch() - this.getMinEnginePitch()) * MathHelper.clamp(normal, 0.0F, 1.25F);
+        this.enginePitch = this.getMinEnginePitch() + (this.getMaxEnginePitch() - this.getMinEnginePitch()) * MathHelper.clamp(this.propellerSpeed / 200F, 0.0F, 2.0F);
         this.engineVolume = this.getControllingPassenger() != null && this.isEnginePowered() ? 0.2F + 0.8F * (this.propellerSpeed / 80F) : 0.001F;
     }
 
