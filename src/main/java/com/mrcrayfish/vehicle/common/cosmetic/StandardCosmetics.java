ebf2cfc3e80751bc7db5358f091ea48b467323e9
MrCrayfish <mrcrayfish@hotmail.com>
2021-10-20 07:29:37 +1030

    📦 Added standard cosmetic defintions

---
+++ src/main/java/com/mrcrayfish/vehicle/common/cosmetic/StandardCosmetics.java
@@ -0,0 +1,30 @@
+package com.mrcrayfish.vehicle.common.cosmetic;
+
+import com.mrcrayfish.vehicle.Reference;
+import net.minecraft.util.ResourceLocation;
+
+/**
+ * Author: MrCrayfish
+ */
+public final class StandardCosmetics
+{
+    public static final ResourceLocation ROOF = new ResourceLocation(Reference.MOD_ID, "roof");
+    public static final ResourceLocation LEFT_DOOR = new ResourceLocation(Reference.MOD_ID, "left_door");
+    public static final ResourceLocation RIGHT_DOOR = new ResourceLocation(Reference.MOD_ID, "right_door");
+    public static final ResourceLocation LEFT_SLIDING_DOOR = new ResourceLocation(Reference.MOD_ID, "left_sliding_door");
+    public static final ResourceLocation RIGHT_SLIDING_DOOR = new ResourceLocation(Reference.MOD_ID, "left_sliding_door");
+    public static final ResourceLocation FRONT_DECOR = new ResourceLocation(Reference.MOD_ID, "front_decor");
+    public static final ResourceLocation TOP_DECOR = new ResourceLocation(Reference.MOD_ID, "top_decor");
+    public static final ResourceLocation REAR_DECOR = new ResourceLocation(Reference.MOD_ID, "rear_decor");
+    public static final ResourceLocation SEAT = new ResourceLocation(Reference.MOD_ID, "seat");
+    public static final ResourceLocation FRONT_SEAT = new ResourceLocation(Reference.MOD_ID, "front_seat");
+    public static final ResourceLocation REAR_SEAT = new ResourceLocation(Reference.MOD_ID, "rear_seat");
+    public static final ResourceLocation DASHBOARD = new ResourceLocation(Reference.MOD_ID, "dashboard");
+    public static final ResourceLocation TRAY = new ResourceLocation(Reference.MOD_ID, "tray");
+    public static final ResourceLocation FRONT_LIGHTS = new ResourceLocation(Reference.MOD_ID, "front_lights");
+    public static final ResourceLocation REAR_LIGHTS = new ResourceLocation(Reference.MOD_ID, "rear_lights");
+    public static final ResourceLocation FRONT_BUMPER = new ResourceLocation(Reference.MOD_ID, "front_bumper");
+    public static final ResourceLocation REAR_BUMPER = new ResourceLocation(Reference.MOD_ID, "rear_bumper");
+    public static final ResourceLocation HOOD = new ResourceLocation(Reference.MOD_ID, "hood");
+    public static final ResourceLocation SPOILER = new ResourceLocation(Reference.MOD_ID, "spoiler");
+}
