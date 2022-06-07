e57fbb34ea9ccc1b1095dc1e74146f3e0d72234d
MrCrayfish <mrcrayfish@hotmail.com>
2022-06-08 00:58:52 +0930

    ✨ Added smoke particle to helicopter exhaust

--- src/main/java/com/mrcrayfish/vehicle/entity/vehicle/CompactHelicopterEntity.java
+++ src/main/java/com/mrcrayfish/vehicle/entity/vehicle/CompactHelicopterEntity.java
@@ -2,6 +2,11 @@ package com.mrcrayfish.vehicle.entity.vehicle;
 
 import com.mrcrayfish.vehicle.entity.HelicopterEntity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.util.math.vector.Vector4f;
 import net.minecraft.world.World;
 
 /**
@@ -12,5 +17,19 @@ public class CompactHelicopterEntity extends HelicopterEntity
     public CompactHelicopterEntity(EntityType<?> entityType, World worldIn)
     {
         super(entityType, worldIn);
+    }
+
+    @Override
+    public void onClientUpdate()
+    {
+        super.onClientUpdate();
+
+        if(this.canDrive() && this.tickCount % 2 == 0)
+        {
+            Vector3d exhaust = this.getExhaustFumesPosition().scale(0.0625);
+            Vector4f fumePosition = new Vector4f(new Vector3f(exhaust));
+            fumePosition.transform(this.getTransformMatrix(0F));
+            this.level.addParticle(ParticleTypes.LARGE_SMOKE, this.getX() + fumePosition.x(), this.getY() + fumePosition.y(), this.getZ() + fumePosition.z(), -this.getDeltaMovement().x, 0.0D, -this.getDeltaMovement().z);
+        }
     }
 }
